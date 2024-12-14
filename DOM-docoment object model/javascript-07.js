9bd31b81c307c3538c805ed516f71a2b9da7f362
md:Anisur rahman <anisuranisur775@gmail.com>
2024-12-15 01:27:35 +0600

    Add files via upload

---
+++ DOM-docoment object model/javascript-07.js
@@ -0,0 +1,39 @@
+/*
+      const p = document.getElementById("paragraph")
+
+
+document.getElementById("clickbtn").addEventListener("click",function(){
+   // console.log("clicked")
+  // alert("clicked")
+  // document.write("hello");
+  p.innerHTML="button is cilicked";
+  p.textContent="btn is clicked";
+})
+      
+
+   const x = document.getElementById("my btn");    // getelementbyid only id dhora jay.
+   p.addEventListener("click", function(){
+
+      alert("button is clicked");
+
+   })
+
+
+
+    document.querySelector("#btn3")    // queryselection diye id class sob dhra jay.
+     // id er jpnne # use korte hy but class er . use korte hy. 
+      btn.addEventListener("click",function(){
+
+       alert("3rd button is clicked");
+
+      })
+       
+         const y =  document.querySelector(".myp")
+
+      function fobtn ()
+      {
+        //alert("4rd is working");
+         myp.textContent = "working good";
+         myp.style.color="red";
+      }
+         */
